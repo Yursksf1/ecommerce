7673e48c449f235a1a495628a5da2cb810a06509
Yurley Sanchez <yurs.ksf1@gmail.com>
2021-05-12 21:00:05 -0500

    implemented models

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Country, City, Address, Category, Product, Imagen
+from .models import Country, City, Address, Category, Product, Imagen, Buy, UserProfile
 
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Address)
 
-
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Imagen)
+
+admin.site.register(Buy)
+admin.site.register(UserProfile)
--- store/models.py
+++ store/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
 import uuid
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    mobile_phone = models.fields.CharField(max_length=50)
+    upload = models.FileField(upload_to='uploads/')
 
 
 class Country(models.Model):
@@ -31,6 +43,11 @@ class Address(models.Model):
         on_delete=models.CASCADE,
     )
     address = models.fields.CharField(max_length=300)
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        null=True
+    )
 
     def __str__(self):
         return '{} - {}'.format(self.address, self.city)
@@ -79,3 +96,21 @@ class Imagen(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Buy(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+    )
+    address = models.ForeignKey(
+        Address,
+        on_delete=models.CASCADE,
+    )
+
