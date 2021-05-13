0142d29bd381af73f9822e031adc76edcf6d2411
Yurley Sanchez <yurs.ksf1@gmail.com>
2021-05-12 20:45:55 -0500

    implemented models of product

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Country
+from .models import Country, City, Address, Category, Product, Imagen
 
 admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Address)
+
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Imagen)
--- store/models.py
+++ store/models.py
@@ -33,4 +33,49 @@ class Address(models.Model):
     address = models.fields.CharField(max_length=300)
 
     def __str__(self):
+        return '{} - {}'.format(self.address, self.city)
+
+
+
+# TODO Refactor models
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+    name = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=300)
+
+    amount = models.fields.PositiveIntegerField()
+    stock = models.fields.PositiveIntegerField()
+
+    active = models.fields.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Imagen(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    upload = models.FileField(upload_to='uploads/')
+    name = models.fields.CharField(max_length=100)
+    active = models.fields.BooleanField(default=True)
+
+    def __str__(self):
         return self.name
