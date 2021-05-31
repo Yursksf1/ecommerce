99ecb5ccfb07ab20875cfcd3fcfddc9c937c9bb8
Yurley Sanchez <yurs.ksf1@gmail.com>
2021-05-31 18:31:32 -0500

    clean code

--- store/serializers.py
+++ store/serializers.py
@@ -2,62 +2,68 @@ from .models import Product, Category, Country, City
 from rest_framework import serializers
 
 
-class ProductListSerializer(serializers.HyperlinkedModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name']
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'stock', 'amount', 'active']
 
-class CreateProductSerializer(serializers.HyperlinkedModelSerializer):
+class CreateProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'stock', 'amount', 'active', 'category_id']
 
 
-class CategoryListSerializer(serializers.HyperlinkedModelSerializer):
+class CategoryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
 
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'description']
 
 
-class CityListSerializer(serializers.HyperlinkedModelSerializer):
+class CityListSerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ['id', 'name']
 
 
-class CitySerializer(serializers.HyperlinkedModelSerializer):
-    country = 'colombis'
-    cities = CityBaseSerializer(many=True, read_only=True)
+
+class CountryBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['id', 'name']
+
+        
+class CitySerializer(serializers.ModelSerializer):
+    country = CountryBaseSerializer()
 
     class Meta:
         model = City
         fields = ['id', 'name', 'country']
 
 
-class CityBaseSerializer(serializers.HyperlinkedModelSerializer):
+class CityBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ['id', 'name']
 
 
-class CountryListSerializer(serializers.HyperlinkedModelSerializer):
+class CountryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ['id', 'name']
+        fields = ['id', 'name', ]
 
 
-class CountrySerializer(serializers.HyperlinkedModelSerializer):
+class CountrySerializer(serializers.ModelSerializer):
     cities = CityBaseSerializer(many=True, read_only=True)
 
     class Meta:
